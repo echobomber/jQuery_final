6515ce4caefd11dd723fd16498b7436c46e4d267
echobomber <jordan990301@g-mail.nsysu.edu.tw>
2020-12-12 11:32:33 +0800

    finish all

--- js/all.js
+++ js/all.js
@@ -1,4 +1,5 @@
 $(function() {
+    //摺疊選單
     $('.subMenu').hide();
     $('.topMenu-link > li > a').on('click', function(e){
         e.preventDefault();
@@ -6,5 +7,9 @@ $(function() {
         $(this).parent().siblings().children('.subMenu').slideUp();
         $(this).toggleClass('active');
         $(this).siblings('.subMenu').slideToggle();
+    })
+    //goTop
+    $('.goTop-btn').on('click', function(e){
+        $('html, body').animate({scrollTop: 0}, 500);
     })
 });
